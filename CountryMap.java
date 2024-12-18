6ed50d2d61b902f71daaf9550255647b89e96b9d
utkubilir <bilirutku35@hotmail.com>
2024-12-18 11:25:08 +0300

    creating time matrix code block in the constructors of countrymap class

--- CountryMap.java
+++ CountryMap.java
@@ -1,8 +1,21 @@
 public class CountryMap {
     private int cityCount;         
-    private City[] cities;          
+    private City[] cities;
+    private int[][] timeMatrix;           
     public CountryMap(int cityCount) {
         this.cityCount = cityCount;
         cities = new City[cityCount];
+        timeMatrix = new int[cityCount][cityCount];
+
+        
+        for (int i = 0; i < cityCount; i++) {
+            for (int j = 0; j < cityCount; j++) {
+                if (i == j) {
+                    timeMatrix[i][j] = 0; // Kendisine mesafe 0
+                } else {
+                    timeMatrix[i][j] = Integer.MAX_VALUE; // Sonsuz
+                }
+            }
+        }
     }
-}
+    }
