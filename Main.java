fa790727aee51f21b911716240f6dcad47401e8b
utkubilir <bilirutku35@hotmail.com>
2024-12-18 11:17:15 +0300

    Creating main class
    I created main class and ı decided to move scanning methods to the main class

---
+++ Main.java
@@ -0,0 +1,14 @@
+import java.io.IOException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner inputScanner = new Scanner(System.in);
+        System.out.println("Enter the absolute path of the input file:");
+        String filePath = inputScanner.nextLine();
+
+        try {
+            Path path = Paths.get(filePath);
+            Scanner fileScanner = new Scanner(path.toFile());
